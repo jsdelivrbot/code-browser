2abf59e46c90ed5c2a3b346480debe98337b9261
Patrick Collins <pscollins@google.com>
2015-12-29 07:11:01 -0500

    Working upload+untar mechanism.

--- lib/App/Code/Browser.pm
+++ lib/App/Code/Browser.pm
@@ -1,4 +1,7 @@
 package App::Code::Browser;
+
+use Archive::Extract;
+use File::Basename;
 use Dancer2;
 use Data::Dumper;
 use Cwd;
@@ -8,13 +11,30 @@ our $VERSION = '0.1';
 # TODO(pscollins): this should probably go in config.yaml
 my $DATA_DIR = getcwd . "/data";
 
+sub inData {
+    my $end = shift;
+    "$DATA_DIR/$end";
+};
+
+sub untar {
+    my ($uploaded, $projname) = @_;
+    my $toPath = inData($projname);
+    debug $toPath;
+    # Need to preprocess in case we got something with ..
+    my $archive = Archive::Extract->new(archive => $uploaded->tempname);
+    mkdir $toPath;
+    $archive->extract(to => $toPath);
+    debug "Extracted to:" . $archive->extract_path;
+};
+
+sub getDirectories {
+    opendir(my $dh, $DATA_DIR) || die "Error reading $DATA_DIR!";
+    grep { !/^\./ && -d "$DATA_DIR/$_" } readdir $dh;
+};
+
 get '/' => sub {
     my %templateVars = (
-        files => [
-            "foo",
-            "bar", 
-            "baz",
-        ],
+        files => [ getDirectories() ],
     );
     my $here = getcwd;
     debug "You are $here";
@@ -22,9 +42,32 @@ get '/' => sub {
 };
 
 post '/upload' => sub {
-    my $ps = request->params();
-    debug Dumper($ps);
-    return 'This will display the upload progress for ' . $ps->{'projname'};
+    delayed {
+        my $name = param('projname');
+        my $tar = upload('tarball');
+        debug Dumper(request->uploads);
+        debug Dumper(request->params);
+        debug Dumper(upload('tarball'));
+        debug "Name: $name";
+
+        content "Uploading... $name";
+        flush;
+
+        untar($tar, $name);
+        content 'Done!';
+
+        done;
+    }
 };
+
+get '/view/**' => sub {
+    # We don't need to sanitize because '/view/..' is interpeted as '/'
+    my ($pathElementRef) = splat;
+    my @pathElements = @{$pathElementRef};
+    my $path = inData(join("/", @pathElements));
+    debug $path;
+    return $path;
+};
+
 
 true;
