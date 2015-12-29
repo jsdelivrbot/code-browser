386194723ecc654164fa6955d842a3685a3816ba
Patrick Collins <pscollins@google.com>
2015-12-29 00:28:05 -0500

    Start to flesh out dir-reading capability.

--- lib/App/Code/Browser.pm
+++ lib/App/Code/Browser.pm
@@ -1,10 +1,30 @@
 package App::Code::Browser;
 use Dancer2;
+use Data::Dumper;
+use Cwd;
 
 our $VERSION = '0.1';
 
+# TODO(pscollins): this should probably go in config.yaml
+my $DATA_DIR = getcwd . "/data";
+
 get '/' => sub {
-    template 'index';
+    my %templateVars = (
+        files => [
+            "foo",
+            "bar", 
+            "baz",
+        ],
+    );
+    my $here = getcwd;
+    debug "You are $here";
+    template('index', \%templateVars);
+};
+
+post '/upload' => sub {
+    my $ps = request->params();
+    debug Dumper($ps);
+    return 'This will display the upload progress for ' . $ps->{'projname'};
 };
 
 true;
